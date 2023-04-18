dc3c0f85f3ba9b34161c0b1c8b95dc9e1ee9ac8c
MD ADNAN HOSSAIN <Adnan.Hossain@iths.se>
2023-04-18 11:53:42 +0200

    doing

--- High-Low Game/Program.cs
+++ High-Low Game/Program.cs
@@ -5,71 +5,270 @@ namespace MyGame
     { 
      
         static void Main(string[] args) 
-        { 
-            Random random = new Random();
-            bool playAgain = true; 
-            int min = 1; 
-            int max = 100;
-            int guess;
-            int number; 
-            int guesses; 
-            String response;
-            while (playAgain)
+        {
+            
+
+                Console.WriteLine("välkommen till HiLo Spel ");
+
+            string level, input;
+
+            Console.WriteLine("Chose your level:L=Lätt, M=Mellan,S=Svårt ");
+            
+
+            level = Console.ReadLine();
+
+            if (level == "L")
+
             {
-                guess = 0;
-                guesses = 0;
-                response = ""; 
-                number = random.Next(min, max + 1);
 
-                while (guess != number) 
+                Random random = new Random();
+                bool playAgain = true;
+                int min = 1;
+                int max = 10;
 
-                { Console.WriteLine("Guess a number between " + min + " - " + max + " : "); 
+                int guess;
+                int number;
+                int guesses;
+                String response;
+                while (playAgain)
 
-                    guess = Convert.ToInt32(Console.ReadLine()); 
-                    
-                    Console.WriteLine("Guess: " + guess);
 
-                    if (guess > number)
+                {
+                    guess = 0;
+                    guesses = 0;
+                    response = "";
+                    number = random.Next(min, max + 1);
 
-                    { 
-                        Console.WriteLine(guess + " is to high!"); 
+                    while (guess != number)
 
+                    {
+                        Console.WriteLine("Gissa på ett tala mellan " + min + " - " + max + " : ");
+
+                        guess = Convert.ToInt32(Console.ReadLine());
+
+                        Console.WriteLine("Guess: " + guess);
+
+                        if (guess > number)
+
+                        {
+                            Console.WriteLine(guess + " Gissningen var för håg!");
+
+                        }
+                        else if (guess < number)
+
+                        {
+                            Console.WriteLine(guess + " Gissningen var för låg!");
+
+                        }
+                        guesses++;
                     }
-                    else if (guess < number)
+                    Console.WriteLine("Number: " + number);
 
-                    { 
-                        Console.WriteLine(guess + " is to low!"); 
+                    Console.WriteLine("YOU WIN!");
 
-                    } guesses++;
-                } 
-                Console.WriteLine("Number: " + number);
-                
-                Console.WriteLine("YOU WIN!"); 
+                    Console.WriteLine("Guesses: " + guesses);
 
-                Console.WriteLine("Guesses: " + guesses);
-                
-                Console.WriteLine("Would you like to play again (Y/N): ");
+                    Console.WriteLine("Would you like to play again (Y/N): ");
 
 
-                response = Console.ReadLine();
+                    response = Console.ReadLine();
 
-                response = response.ToUpper(); 
-                
-                if (response == "Y")
+                    response = response.ToUpper();
 
-                { 
-                    playAgain = true; 
-                } 
-                else
-                { 
-                    playAgain = false;
+                    if (response == "Y")
+
+                    {
+                        playAgain = true;
+                    }
+                    else
+                    {
+                        playAgain = false;
+                    }
+
                 }
 
-            } 
+
+                Console.WriteLine("Thanks for playing the Game! ... ");
+
+                Console.ReadKey();
+            }
+
+            else if (level == "M")
+                    {
+                        Random random = new Random();
+                        bool playAgain = true;
+                        int min = 10;
+                        int max = 20;
+
+                        int guess;
+                        int number;
+                        int guesses;
+                       String response;
+                        while (playAgain)
+
+
+                        {
+                            guess = 0;
+                            guesses = 0;
+                            response = "";
+                            number = random.Next(min, max + 1);
+
+                            while (guess != number)
+
+                            {
+                                Console.WriteLine("Guess a number between " + min + " - " + max + " : ");
+
+                                guess = Convert.ToInt32(Console.ReadLine());
+
+                                Console.WriteLine("Guess: " + guess);
+
+                                if (guess > number)
+
+                                {
+                                    Console.WriteLine(guess + " Gissningen var för håg!");
+                                    
+
+                                }
+                                else if (guess < number)
+
+                                {
+                                    Console.WriteLine(guess + " Gissningen var för låg!");
+
+                                }
+                                guesses++;
+                            }
+                            Console.WriteLine("Number: " + number);
+
+                            Console.WriteLine("YOU WIN!");
+
+                            Console.WriteLine("Guesses: " + guesses);
+
+                            Console.WriteLine("Would you like to play again (Y/N): ");
+
+
+                            response = Console.ReadLine();
+
+                            response = response.ToUpper();
+
+                            if (response == "Y")
+
+                            {
+                                playAgain = true;
+                            }
+                            else
+                            {
+                                playAgain = false;
+                            }
+
+                        }
+
+
+                        Console.WriteLine("Thanks for playing the Game! ... ");
+
+                        Console.ReadKey();
+
+
+                        }
+            else _ = (level == "S");
+                        {
+                            Random random = new Random();
+                            bool playAgain = true;
+                            int min = 20;
+                            int max = 30;
+
+                            int guess;
+                            int number;
+                            int guesses;
+                           String response;
+                            while (playAgain)
+
+
+                            {
+                                guess = 0;
+                                guesses = 0;
+                                response = "";
+                                number = random.Next(min, max + 1);
+
+                                while (guess != number)
+
+                                {
+                                    Console.WriteLine("Gissa på ett tala mellan " + min + " - " + max + " : ");
+
+                                    guess = Convert.ToInt32(Console.ReadLine());
+
+                                    Console.WriteLine("Guess: " + guess);
+
+                                    if (guess > number)
+
+                                    {
+                                        Console.WriteLine(guess + " Gissningen var för håg!");
+
+                                    }
+                                    else if (guess < number)
+
+                                    {
+                                        Console.WriteLine(guess + " Gissningen var för låg!");
+
+                                    }
+                                    guesses++;
+                                }
+                                Console.WriteLine("Number: " + number);
+
+                                Console.WriteLine("YOU WIN!");
+
+                                Console.WriteLine("Guesses: " + guesses);
+
+                                Console.WriteLine("Would you like to play again (Y/N): ");
+
+
+                                response = Console.ReadLine();
+
+                                response = response.ToUpper();
+
+                                if (response == "Y")
+
+                                {
+                                playAgain = true;
+                               }
+                                else
+                                {
+                                playAgain = false;
+                            }
+
+
+                            Console.WriteLine("Would you like to play again (Y/N): ");
+
+
+                            response = Console.ReadLine();
+
+                            response = response.ToUpper();
+
+                            if (response == "Y")
+
+                            {
+                                playAgain = true;
+                            }
+                            else
+                            {
+
+                                playAgain = false;
+                            }
+
+
+
+                        }
+                    }
+
+            Console.WriteLine("Thanks for playing the Game! ... ");
+
+                Console.ReadKey();
             
-            Console.WriteLine("Thanks for playing! ... I guess");
-            
-            Console.ReadKey();
+               
+
+
+
+
+
         }
     }
 }
